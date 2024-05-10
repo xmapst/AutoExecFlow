bf07d99e1dd88ba757736f39d4123f1ad9f55232
xmapst <xmapst@gmail.com>
2024-05-10 12:00:04 +0800

    update

--- pkg/exec/exec_orther.go
+++ pkg/exec/exec_orther.go
@@ -39,8 +39,7 @@ func (c *Cmd) beforeExec() []func(cmd *exec.Cmd) {
 	return []func(cmd *exec.Cmd){
 		func(cmd *exec.Cmd) {
 			cmd.SysProcAttr = &syscall.SysProcAttr{
-				Setpgid:   true,
-				Pdeathsig: syscall.SIGKILL,
+				Setpgid: true,
 			}
 		},
 	}
