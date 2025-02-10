70da22cb9d93a829512bd68b779a47bcbfa5c454
xmapst <xmapst@gmail.com>
2025-02-10 18:27:11 +0800

    update update

--- internal/worker/runner/exec/exec.go
+++ internal/worker/runner/exec/exec.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/segmentio/ksuid"
+	"github.com/xmapst/logx"
 
 	"github.com/xmapst/AutoExecFlow/internal/storage"
 	"github.com/xmapst/AutoExecFlow/internal/worker/common"
@@ -20,8 +21,9 @@ import (
 type SCmd struct {
 	storage storage.IStep
 
-	ctx    context.Context
-	cancel context.CancelFunc
+	ctx     context.Context
+	cancel  context.CancelFunc
+	envPath string
 
 	shell      string
 	workspace  string
@@ -85,6 +87,66 @@ func (c *SCmd) envs() []string {
 		fmt.Sprintf("TASK_STEP_NAME=%s", c.storage.Name()),
 		fmt.Sprintf("TASK_WORKSPACE=%s", c.workspace),
 	)...)
+}
+
+func (c *SCmd) parseEnvFileFromFile() {
+	// 打开源文件
+	file, err := os.Open(c.envPath)
+	if err != nil {
+		logx.Warnln(err)
+		return
+	}
+	defer file.Close()
+	s := bufio.NewScanner(file)
+	firstLine := true
+	for s.Scan() {
+		line := s.Text()
+
+		if firstLine {
+			firstLine = false
+			// skip utf8 bom, powershell 5 legacy uses it for utf8
+			if len(line) >= 3 && line[0] == 239 && line[1] == 187 && line[2] == 191 {
+				line = line[3:]
+			}
+		}
+
+		// 处理单行和多行环境变量
+		singleLineEnv := strings.Index(line, "=")
+		multiLineEnv := strings.Index(line, "<<")
+		if singleLineEnv != -1 && (multiLineEnv == -1 || singleLineEnv < multiLineEnv) {
+			// TODO: write to storage
+			logx.Debugf("parsed env: %v=%v", line[:singleLineEnv], line[singleLineEnv+1:])
+		} else if multiLineEnv != -1 {
+			multiLineEnvContent := ""
+			multiLineEnvDelimiter := line[multiLineEnv+2:]
+			delimiterFound := false
+			for s.Scan() {
+				content := s.Text()
+				if content == multiLineEnvDelimiter {
+					delimiterFound = true
+					break
+				}
+				if multiLineEnvContent != "" {
+					multiLineEnvContent += "\n"
+				}
+				multiLineEnvContent += content
+			}
+			if !delimiterFound {
+				logx.Errorf("invalid format delimiter '%v' not found before end of file", multiLineEnvDelimiter)
+				return
+			}
+			// TODO: write to storage
+			logx.Debugf("parsed env: %v=%v", line[:multiLineEnv], multiLineEnvContent)
+		} else {
+			logx.Errorf("invalid format '%v', expected a line with '=' or '<<'", line)
+			return
+		}
+	}
+
+	if err = s.Err(); err != nil {
+		logx.Errorf("error reading file: %v", err)
+		return
+	}
 }
 
 func (c *SCmd) newCmd(ctx context.Context) (*exec.Cmd, error) {
